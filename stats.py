f20b09f8eeddbcd854949243c00cfec722a192fc
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-10-18 00:10:37 +0200

    Update date

--- stats.py
+++ stats.py
@@ -128,7 +128,10 @@ def main(argv):
         b = 1
         for t in horodatage[s[0]]:
             print '\t-> Ban %02d' % b,
-            print 'à', t[0] + ' pour ' + str(t[1]) + ' minutes' + ' (' + t[2] + ')'
+            if len(t[0]) > 8:
+                print 'à', t[0][:-8] + ' pour ' + str(t[1]) + ' minutes' + ' (' + t[2] + ')'
+            else
+                print 'à', t[0] + ' pour ' + str(t[1]) + ' minutes' + ' (' + t[2] + ')'
             b += 1
 
         print 'Total\t=>',
