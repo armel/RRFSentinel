06ca4c29c3e26015a717d85ba63f5cf411a36e63
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-13 23:52:17 +0200

    Fix bug

--- settings.py
+++ settings.py
@@ -16,9 +16,9 @@ On ban pour 5 minutes
 
 Campeur
 
-Sur une plage d'une durée de 30 minutes
-Si le nombre de passage en émission est supérieur ou égale à 15
-Et que le temps d'émission est supérieur ou égale à 600 secondes (10 minutes)
+Sur une plage d'une durée de 60 minutes
+Si le nombre de passage en émission est supérieur ou égale à 30
+Et que le temps d'émission est supérieur ou égale à 1800 secondes (30 minutes)
 On ban pour 15 minutes
 '''
 
@@ -35,10 +35,10 @@ intempestif_ban = 5             # durée de la quarantaine en minutes
 intempestif_tx = 4              # nombre max de déclenchements suspects 
 intempestif_fair_use = 3        # nombre de ban avant application de la règle strict
 
-campeur_plage = 30              # durée de la plage de déclenchements en minutes
+campeur_plage = 60              # durée de la plage de déclenchements en minutes
 campeur_ban = 15                # durée de la quarantaine en minutes
-campeur_tx = 15                 # nombre max de passage en émission
-campeur_bf = 600                # nombre max de seconde en émission
+campeur_tx = 30                 # nombre max de passage en émission
+campeur_bf = 1800               # nombre max de seconde en émission
 campeur_fair_use = 1            # nombre de ban avant application de la règle strict
 
 fair_use_time = '06:00' # heure de ban avant application de la règle strict
