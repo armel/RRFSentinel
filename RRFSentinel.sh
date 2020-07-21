616979c942a599c675c5d17bfbf72d6b7006c6c8
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-07-21 04:26:58 +0200

    Refactoring

--- RRFSentinel.sh
+++ RRFSentinel.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 PATH_SCRIPT='/root/RRFSentinel/RRFSentinel.py'
+PATH_LOG='/root'
 PATH_PID='/root'
 
 case "$1" in
     start)
         echo "Starting RRFSentinel"
-        nohup python3 $PATH_SCRIPT 2>&1 & echo $! > $PATH_PID/RRFSentinel.pid
+        nohup python3 $PATH_SCRIPT > /dev/null 2>&1 & echo $! > $PATH_PID/RRFSentinel.pid
         ;;
     stop) 
         echo "Stopping RRFSentinel"
