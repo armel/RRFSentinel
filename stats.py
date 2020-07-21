9f8e60eac7f30cb4a5497e77c890afb231c9e2e7
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-07-21 05:46:02 +0200

    Update stats

--- stats.py
+++ stats.py
@@ -100,8 +100,8 @@ def main(argv):
                 if '>> sudo' in line:
                     element = line.split(' - ')
                     if len(element) == 7:
-                        intempestif_stat = save_stat(intempestif_stat, element[6], int(element[2]))
-                        intempestif_horodatage = save_horodatage(intempestif_horodatage, element[6], element[1], int(element[2]), element[7])
+                        intempestif_stat = save_stat(intempestif_stat, element[5], int(element[2]))
+                        intempestif_horodatage = save_horodatage(intempestif_horodatage, element[5], element[1], int(element[2]), element[6])
                     elif len(element) == 6:
                         campeur_stat = save_stat(campeur_stat, element[1], int(element[3]))
                         campeur_horodatage = save_horodatage(campeur_horodatage, element[1], element[0], int(element[3]), element[4])
