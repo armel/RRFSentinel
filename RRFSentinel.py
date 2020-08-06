8556139b1e7cb32f1c0161dbdd04d9e5453585a3
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-08-06 04:50:05 +0200

    Node liste refactoring

--- RRFSentinel.py
+++ RRFSentinel.py
@@ -105,8 +105,9 @@ def main(argv):
                             if h[t] >= plage_start_campeur and h[t] < plage_stop:
                                 tx += 1
                                 bf += l.convert_time_to_second(c[t])
-                                print(data['Indicatif'], h[t], c[t], tx, bf)
+                                #print(data['Indicatif'], h[t], c[t], tx, bf)
                         if tx >= s.campeur_tx and bf >= s.campeur_bf:
+                            print('ici')
                             ban_start = plage_stop
                             ban_timestamp = (now + datetime.timedelta(minutes = s.campeur_ban))
                             ban_stop = ban_timestamp.strftime('%H:%M:%S')
